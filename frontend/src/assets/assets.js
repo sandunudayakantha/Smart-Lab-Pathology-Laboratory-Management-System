a88ecef025a2be0b57c06f2ca2a7d85205cdc131
Sandun Udaya Kantha <sandunudayakantha.eg@gmail.com>
2026-02-20 23:25:15 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,6 +1,6 @@
 import appointment_img from './appointment_img.png'
 import header_img from './header_img.png'
-import group_profiles from './group_profiles.png'
+//import group_profiles from './group_profiles.png'
 import profile_pic from './profile_pic.png'
 import contact_image from './contact_image.png'
 import about_image from './about_image.png'
